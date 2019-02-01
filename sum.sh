8bbea0cee9bd4ebb9fad65a021981ade5ccfb18a
Shri <shridharan.devops@gmail.com>
2019-02-01 13:21:31 +0530

    yoo

--- sum.sh
+++ sum.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # it is a shell script for addition of two numbers
-trying to merge branch with master
-starting jenkins
-hoja be 
+#trying to merge branch with master
+#starting jenkins
+#hoja be 
 echo "enter first numbers: "
 read x 
 echo "enter second number: "
 read y
 sum=$(expr "$x" + "$y")
 echo $x + $y = $sum
+echo the sum is $(( x + y ))
 
